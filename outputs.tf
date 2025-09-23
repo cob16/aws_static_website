9eb2b1dacf57676955f68c0586168e7b86adc390
cob16 <public+github@cormacbrady.info>
2025-09-23 02:24:55 +0100

    add backup email alerting method

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "aws_cloudfront_distribution" {
 output "s3_bucket_name" {
   value = module.static_website.s3_bucket_name
 }
+
+output "email_pending_confirmation" {
+  value = aws_sns_topic_subscription.email_alerts.pending_confirmation
+}
