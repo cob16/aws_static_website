fccc93298095a9c4dcd857ecab751d6eb210cf91
cob16 <public+github@cormacbrady.info>
2025-09-23 02:40:29 +0100

    update providers

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.93.0"
+      version = "~> 6.14.1"
     }
     pagerduty = {
       source  = "pagerduty/pagerduty"
-      version = "~> 2.0"
+      version = "~> 3.29"
     }
   }
 
